59aed69275af71f5ec699c6603149cd57fc282c0
Brett Zamir <brettz9@yahoo.com>
2020-01-09 12:54:19 +0800

    - Breaking change: Check argument to script (`process.argv`) instead of environmental variable for port (recommended to avoid env. vars.: https://eslint.org/docs/rules/no-process-env ) - Linting (ESLint): Switch to ash-nazg config (which inherits eslint:recommended); lint any HTML/Markdown JavaScript
    Though mostly stylistic changes, also pointed to `body-parser` needing to be in deps.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,7 @@
 module.exports = {
-  extends: ["eslint:recommended"],
-  env: {
-    node: true
-  },
+  extends: ['ash-nazg/sauron-node', 'plugin:node/recommended-script'],
   rules: {
-    semi: ['error'],
-    indent: ['error', 2]
+    'import/no-commonjs': 0,
+    'node/exports-style': 0
   }
 };
--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 1844,
+'use strict';
+
+const express = require('express'),
   bodyParser = require('body-parser'),
   RateLimit = require('express-rate-limit');
+
+const routes = require('./api/routes/bDateRoutes'); // importing route
+
+const app = express(),
+  port = process.argv[2] || 1844;
 
 app.use(new RateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
   max: 20
 }));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var routes = require('./api/routes/bDateRoutes'); //importing route
-routes(app); //register the route
+routes(app); // register the route
 
 app.listen(port);
 
+// eslint-disable-next-line no-console
 console.log('Baha\'i Date RESTful API server started: Port ' + port);
