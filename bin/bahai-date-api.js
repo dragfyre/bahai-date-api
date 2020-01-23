db872395b3ab0c1e06060896ff7098817b3bc068
Brett Zamir <brettz9@yahoo.com>
2020-01-23 23:34:23 +0800

    - Enhancement: Add CLI for displaying date in plain text only or JSON - Point `main` to controllers file (which has potentially usable utilities) - License: Add badi-cal (MIT) license

---
+++ bin/bahai-date-api.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+'use strict';
+
+const {join} = require('path');
+const {cliBasics} = require('command-line-basics');
+const {
+  getDate, getTodayJSON
+} = require('../api/controllers/bDateController.js');
+
+const optionDefinitions = cliBasics(
+  join(__dirname, './optionDefinitions.js')
+);
+
+if (!optionDefinitions) { // cliBasics handled
+  process.exit(); // eslint-disable-line no-process-exit
+}
+
+const {date, verbose} = optionDefinitions;
+
+let dateObj;
+if (date) {
+  const dte = new Date(date);
+  dateObj = getDate({
+    year: dte.getFullYear(),
+    month: dte.getMonth(),
+    day: dte.getDate(),
+    hour: dte.getHours(),
+    minute: dte.getMinutes(),
+    second: dte.getSeconds()
+  });
+} else {
+  dateObj = getTodayJSON();
+}
+
+const output = verbose ? dateObj : dateObj.json.message;
+// eslint-disable-next-line no-console
+console.log(output);
