34d122b05bcea68f130e56a7c22eef18864231fe
Brett Zamir <brettz9@yahoo.com>
2022-04-09 15:57:30 +0800

    feat: Make rate limit optional and configurable

--- createServer.js
+++ createServer.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import rateLimit from 'express-rate-limit';
+import expressRateLimit from 'express-rate-limit';
 
 import routes from './api/routes/bDateRoutes.js'; // importing routes
 
@@ -8,11 +8,16 @@ import routes from './api/routes/bDateRoutes.js'; // importing routes
  * @param {ExpressApp} app
  * @returns {ExpressApp}
  */
-function createServer (app = express()) {
-  app.use(rateLimit({
+function createServer ({
+  app = express(),
+  rateLimit = {
     windowMs: 1 * 60 * 1000, // 1 minute
     max: 20
-  }));
+  }
+} = {}) {
+  if (rateLimit) {
+    app.use(expressRateLimit(rateLimit));
+  }
 
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
